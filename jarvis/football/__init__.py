98029b58329069b8217bec81e42cdc0bd7836809
wujoshnjr <a0905271182@gmail.com>
2026-08-18 07:01:32 +0800

    Export fixture context research API

--- jarvis/football/__init__.py
+++ jarvis/football/__init__.py
@@ -1,3 +1,12 @@
+from .context import (
+    CONGESTION_HOURS,
+    FOOTBALL_CONTEXT_FAMILY,
+    FOOTBALL_CONTEXT_VERSION,
+    FixtureContextSnapshot,
+    build_context_residual_observation,
+    build_fixture_context_snapshot,
+    fixture_context_numeric_features,
+)
 from .strength import (
     DYNAMIC_STRENGTH_VERSION,
     DynamicStrengthFit,
@@ -15,15 +24,22 @@ from .tuning import (
 )
 
 __all__ = [
+    "CONGESTION_HOURS",
     "DYNAMIC_STRENGTH_VERSION",
     "DYNAMIC_STRENGTH_TUNING_VERSION",
+    "FOOTBALL_CONTEXT_FAMILY",
+    "FOOTBALL_CONTEXT_VERSION",
     "DynamicStrengthCandidateResult",
     "DynamicStrengthFit",
     "DynamicStrengthObservation",
     "DynamicStrengthPrediction",
     "DynamicStrengthTuningResult",
     "DynamicStrengthValidationFixture",
+    "FixtureContextSnapshot",
+    "build_context_residual_observation",
+    "build_fixture_context_snapshot",
     "fit_dynamic_strength",
+    "fixture_context_numeric_features",
     "predict_dynamic_lambdas",
     "tune_dynamic_strength",
 ]
