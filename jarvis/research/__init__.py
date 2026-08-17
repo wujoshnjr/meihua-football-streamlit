00e5311b7a5f70390e03b93fc3c7243ed4351ff7
wujoshnjr <a0905271182@gmail.com>
2026-08-18 00:30:06 +0800

    Export experiment engine APIs

--- jarvis/research/__init__.py
+++ jarvis/research/__init__.py
@@ -1,3 +1,14 @@
+from .experiment import (
+    EXPERIMENT_SCHEMA_VERSION,
+    ForecastEvaluation,
+    ModelForecast,
+    PrematchExperimentRecord,
+    SnapshotRef,
+    aggregate_evaluations,
+    evaluate_forecast,
+    paired_model_comparison,
+    validate_chronological_dataset,
+)
 from .residual import (
     GENERIC_RESIDUAL_FIT_VERSION,
     ResidualLambdaFit,
@@ -7,9 +18,18 @@ from .residual import (
 )
 
 __all__ = [
+    "EXPERIMENT_SCHEMA_VERSION",
+    "ForecastEvaluation",
     "GENERIC_RESIDUAL_FIT_VERSION",
+    "ModelForecast",
+    "PrematchExperimentRecord",
     "ResidualLambdaFit",
     "ResidualLambdaObservation",
+    "SnapshotRef",
+    "aggregate_evaluations",
     "apply_residual_lambda_adjustment",
+    "evaluate_forecast",
     "fit_residual_lambda_adjustment",
+    "paired_model_comparison",
+    "validate_chronological_dataset",
 ]
