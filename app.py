839d92f4cc29e2caf4091e38026a2c2f821459c4
wujoshnjr <a0905271182@gmail.com>
2026-08-27 20:27:44 +0800

    10.4: rename football workflow navigation

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 )
 
 home = st.Page("pages/00_Home.py", title="JARVIS", icon="🏠", default=True)
-football_case = st.Page("pages/5_Football_Case.py", title="足球雙術數", icon="⚽", url_path="football-case")
+football_case = st.Page("pages/5_Football_Case.py", title="足球多層案件", icon="⚽", url_path="football-case")
 qimen = st.Page("pages/1_Qimen_Cast.py", title="奇門起局", icon="🧭", url_path="qimen")
 meihua = st.Page("pages/2_Meihua_Cast.py", title="梅花起卦", icon="☯️", url_path="meihua")
 yuanling = st.Page("pages/6_Yuanling_Yanshu.py", title="元靈演數", icon="🔢", url_path="yuanling")
